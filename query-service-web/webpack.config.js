b1f24e719f229ce7e211ea7ea94b80eeb81c4b48
bdgou <ben@gould.one>
2026-01-08 14:43:10 -0500

    refined pom for distro and then fixed webpack

--- query-service-web/webpack.config.js
+++ query-service-web/webpack.config.js
@@ -1,24 +1,17 @@
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        include: /node_modules\/monaco-editor/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              url: false // Don't resolve URLs in CSS files
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(ttf|woff|woff2|eot)$/,
-        include: /node_modules\/monaco-editor/,
-        type: 'asset/resource'
-      }
-    ]
-  }
+module.exports = (config) => {
+  // Add monaco-editor rules at the beginning so they're matched first
+  config.module.rules.unshift(
+    {
+      test: /\.css$/,
+      include: /node_modules[\\/]monaco-editor/,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.(ttf|woff|woff2|eot)$/,
+      include: /node_modules[\\/]monaco-editor/,
+      type: 'asset/resource'
+    }
+  );
+
+  return config;
 };
